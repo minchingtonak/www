8e243103ef422328c5b86624912c9020be26c3a1
akminch <akminch@umich.edu>
2019-07-23 17:54:01 -0400

    Better height resizing, device size transition

--- main.js
+++ main.js
@@ -31,10 +31,26 @@ $(document).ready(function () {
     $('.html-body').css('height', 'unset');
 
     // Resize the title page if the window size changes
+    var state = get_device_state();
+    var prev_state = state;
+    var prev_height = $(window).height();
     $(window).resize(function () {
-        $('.html-body').css('height', '100%');
-        $('#title-container').css('height', '100%');
-        $('#title-container').css('height', $("#navbar").offset().top);
-        $('.html-body').css('height', 'unset');
+        prev_state = state;
+        state = get_device_state();
+
+        // Fix height of title page on window vertical resize
+        if ($(window).height() != prev_height) {
+            $('.html-body').css('height', '100%');
+            $('#title-container').css('height', '100%');
+            $('#title-container').css('height', $("#navbar").offset().top);
+            $('.html-body').css('height', 'unset');
+            prev_height = $(window).height();
+        }
+
+        // When switching between mobile and desktop, hide/show navbar buttons accordingly
+        if (!is_mobile())
+            $('#navbar-container').show();
+        else if (state == 'mobile' && prev_state == 'desktop' && $('#navbar-container').css('display') != 'none')
+            $('#navbar-container').css('display', 'none');
     });
 });
